e4d89c9dd77a144fd2c073579b84f3c907bbe92b
siddhi-upadhyay83 <siddhiupadhyay90@gmail.com>
2026-05-28 13:32:01 +0530

    added a new code

---
+++ Day-1/count digits in a number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+  int n;
+  printf("enter a number\n");
+  scanf("%d ",&n);
+  int count=0;
+  while (n!=0){
+    n=n/10;
+    count++;
+  }
+  printf("the number of digits are=%d",count);
+  return 0;
+
+}
