dc9647138ee02cd2677dbafec2be883c2fb19d3d
siddhi-upadhyay83 <siddhiupadhyay90@gmail.com>
2026-06-08 09:17:05 +0530

    day-11 of ques 41 code is completed

---
+++ Day-11/q41.c
@@ -0,0 +1,14 @@
+//41.wap to print sum of two number//
+#include<stdio.h>
+int sum(int a,int b){
+  return a+b;
+}
+int main(){
+  int a,b,sum;
+  printf("enter two numbers\n");
+  scanf("%d %d",&a,&b);
+  sum=a+b;
+  printf("sum of %d and %d=%d",a,b,sum);
+  return 0;
+}
+
